4b9c70697e9d9c1fc41cba8e410eb2e747aeb09c
sam-122333 <106857494+sam-122333@users.noreply.github.com>
2023-04-12 12:43:45 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// counter design
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 1287, 2000);
+  counter("count2", 0, 2435, 2000);
+  counter("count3", 0, 1114, 2000);
+  counter("count4", 0, 2121, 2000);
+});
+
+//  active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+};
+
+// nav hide
+// let navBar = document.querySelector(".nav-link");
+// let navCollapse = document.querySelector(".nav-collapse.collapse");
+// navBar.forEach(function (a) {
+//   a.addEventListener("click", function () {
+//     navCollapse.classList.remove("show");
+//   });
+// });
+
+document.ready(function () {
+  // Close Navbar on Click in Mobile View
+  ".navbar-nav li a".click(function (event) {
+    ".navbar-collapse".collapse("hide");
+  });
+});
